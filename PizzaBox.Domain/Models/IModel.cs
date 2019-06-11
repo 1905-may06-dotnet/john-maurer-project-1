615264dc05fdc1e5a77372c4ea533e4872b11189
john-maurer <jmau0438@gmail.com>
2019-06-11 10:54:40 -0700

    pipeline fix

--- PizzaBox.Domain/Models/IModel.cs
+++ PizzaBox.Domain/Models/IModel.cs
@@ -4,19 +4,9 @@ using System.Text;
 
 namespace PizzaBox.Domain.Models {
 
-    public enum Status {
-
-        Unchanged = 1,
-        Updated   = 2,
-        Deleted   = 3
-
-    }
-
     public abstract class IModel < T > {
 
         protected T _resource;
-
-        public Status HasChanged = Status.Unchanged;
 
     }
 
